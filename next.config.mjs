dee0c658f73bdaa7d5541055f881a62347e47516
Tino Breddin <tino@hoprnet.org>
2022-11-28 14:32:50 +0100

    Update Next.JS configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 // Next.js bundler runs in ESM mode whereas
 // Next.js web server runs in CommonJS mode
 // @TODO remove package.json once Next.js web server supports ESM mode
-export default {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   env: {
     NEXT_PUBLIC_GIT_COMMIT: process.env.HOPR_GIT_COMMIT
-  }
+  },
 }
+
+module.exports = nextConfig
