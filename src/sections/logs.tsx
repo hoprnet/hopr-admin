055288a3526e0a08d20c9915ae6e82d8c3ac0a5e
camilovegag <camilovegag@unisabana.edu.co>
2023-06-08 11:31:23 -0500

    Remove useEffect & get logs from redux

--- src/sections/logs.tsx
+++ src/sections/logs.tsx
@@ -1,42 +1,12 @@
-import { useEffect, useState } from 'react';
 import { useAppSelector } from '../store';
-import { utils } from '@hoprnet/hopr-sdk';
-import Section from '../future-hopr-lib-components/Section';
-import readStreamEvent from '../utils/readStreamEvent';
-import { Log } from '../types';
 import LogLine from '../components/LogLine';
+import Section from '../future-hopr-lib-components/Section';
 
 function SectionLogs() {
-  const [logs, set_logs] = useState<Log[]>([]);
   const { apiEndpoint, apiToken } = useAppSelector(
     (selector) => selector.auth.loginData
   );
-
-  // TODO: decodeMessage & path on next SDK update.
-  useEffect(() => {
-    if (apiEndpoint && apiToken) {
-      console.log('Mounting component');
-      const ws = new utils.WebsocketHelper({
-        apiEndpoint,
-        apiToken,
-        decodeMessage: false,
-        path: '/api/v2/node/stream/websocket/',
-        onOpen() {
-          console.log('ws connection open');
-        },
-        onMessage(data) {
-          const log = readStreamEvent(data);
-          if (log) {
-            set_logs((prevLogs) => [...prevLogs, log]);
-          }
-        },
-      });
-      return () => {
-        console.log('ws closed');
-        ws.close();
-      };
-    }
-  }, []);
+  const { logs } = useAppSelector((selector) => selector.node);
 
   if (!apiEndpoint || !apiToken) {
     return (
