94b19f0ea4a6acf8e9bc7ead1213f72c025e4053
Diego Alzate <diego35.da@gmail.com>
2023-06-08 11:50:38 -0500

    format

--- src/sections/safe.tsx
+++ src/sections/safe.tsx
@@ -7,7 +7,7 @@ import { useAppDispatch, useAppSelector } from '../store';
 import Section from '../future-hopr-lib-components/Section';
 import { useSigner } from '../hooks';
 import { actionsAsync } from '../store/slices/safe/actionsAsync';
-import { utils } from "ethers"
+import { utils } from 'ethers';
 function SafeSection() {
   const dispatch = useAppDispatch();
   const safe = useAppSelector((store) => store.safe);
@@ -23,38 +23,57 @@ function SafeSection() {
     <Section className="Section--safe" id="Section--safe" yellow>
       <h1>SAFE REDUX STORE</h1>
       <pre>{JSON.stringify(safe, null, 4)}</pre>
-      <h1>
-        exiting safes
-      </h1>
-      {safe.safesByOwner.map(safeAddress => (
-        <button key={safeAddress} onClick={() => {
-          if (signer) {
-            dispatch(actionsAsync.getSafeInfoThunk({ signer, safeAddress }))
-            dispatch(actionsAsync.getAllSafeTransactionsThunk({ signer, safeAddress }))
-          }
-        }}>click to get info from {safeAddress}</button>
-      ))}
-      <h1>
-        create new safe
-      </h1>
-      <button onClick={() => {
-        if (signer) {
-          dispatch(actionsAsync.createSafeThunk({ signer }))
-        }
-      }}>create new default safe</button>
-      <h1>create tx proposal to yourself on selected safe</h1>
-      <button disabled={!safe.selectedSafeAddress} onClick={async () => {
-        if (safe.selectedSafeAddress && signer) {
-          const signerAddress = await signer.getAddress();
-          dispatch(actionsAsync.createSafeTransactionThunk({
-            safeAddress: safe.selectedSafeAddress, signer, safeTransactionData: {
-              value: utils.parseEther("0.001").toString(),
-              to: signerAddress,
-              data: "0x",
+      <h1>exiting safes</h1>
+      {safe.safesByOwner.map((safeAddress) => (
+        <button
+          key={safeAddress}
+          onClick={() => {
+            if (signer) {
+              dispatch(actionsAsync.getSafeInfoThunk({ signer, safeAddress }));
+              dispatch(
+                actionsAsync.getAllSafeTransactionsThunk({
+                  signer,
+                  safeAddress,
+                })
+              );
             }
-          }))
-        }
-      }}>create tx proposal</button>
+          }}
+        >
+          click to get info from {safeAddress}
+        </button>
+      ))}
+      <h1>create new safe</h1>
+      <button
+        onClick={() => {
+          if (signer) {
+            dispatch(actionsAsync.createSafeThunk({ signer }));
+          }
+        }}
+      >
+        create new default safe
+      </button>
+      <h1>create tx proposal to yourself on selected safe</h1>
+      <button
+        disabled={!safe.selectedSafeAddress}
+        onClick={async () => {
+          if (safe.selectedSafeAddress && signer) {
+            const signerAddress = await signer.getAddress();
+            dispatch(
+              actionsAsync.createSafeTransactionThunk({
+                safeAddress: safe.selectedSafeAddress,
+                signer,
+                safeTransactionData: {
+                  value: utils.parseEther('0.001').toString(),
+                  to: signerAddress,
+                  data: '0x',
+                },
+              })
+            );
+          }
+        }}
+      >
+        create tx proposal
+      </button>
     </Section>
   );
 }
