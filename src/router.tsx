36bd0d0ead3faa62a1665aab5d28bcd8d48dd309
camilovegag <camilovegag@unisabana.edu.co>
2023-06-13 12:32:45 -0500

    Add loginNeeded to Logs

--- src/router.tsx
+++ src/router.tsx
@@ -62,6 +62,7 @@ export const applicationMap = [
         path: 'logs',
         icon: <MailIcon />,
         element: <SectionLogs />,
+        loginNeeded: 'node',
       },
       {
         name: 'Info',
