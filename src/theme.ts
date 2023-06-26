3e1c4a67ec2d9e1de7f27bbfac99ad854a650d81
camilovegag <camilovegag@unisabana.edu.co>
2023-06-26 14:35:00 -0500

    Remove old fonts

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material';
 
-const theme = createTheme({ typography: { fontFamily: `'Source Code Pro', monospace` } });
+const theme = createTheme({ typography: { fontFamily: 'Source Code Pro, monospace' } });
 
 export default theme;
