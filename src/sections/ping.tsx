bc80c1396b2520e601b572ab340370a603ca9bb3
camilovegag <camilovegag@unisabana.edu.co>
2023-07-03 09:43:34 -0500

    Create initial ping subpage

---
+++ src/sections/ping.tsx
@@ -0,0 +1,11 @@
+import Section from '../future-hopr-lib-components/Section';
+
+function PingPage() {
+  return (
+    <Section yellow>
+      <h2>Ping</h2>
+    </Section>
+  );
+}
+
+export default PingPage;
