6a3d956246edf2850ca92d80a71ac8eec4cdc13d
camilovegag <camilovegag@unisabana.edu.co>
2023-07-13 14:22:05 -0500

    Current changes

--- src/sections/staking-screen.tsx
+++ src/sections/staking-screen.tsx
@@ -1,13 +1,25 @@
+import { Card, CardContent } from '@mui/material';
 import Section from '../future-hopr-lib-components/Section';
+import { useAppSelector } from '../store';
+import styled from '@emotion/styled';
+
+const StyledCard = styled(Card)`
+  padding: 2rem;
+  max-width: 1080px;
+`;
 
 const StakingScreen = () => {
+  const account = useAppSelector((selector) => selector.web3.account);
   return (
     <Section
       lightBlue
       center
       fullHeightMin
     >
-      <p>Staking screen</p>
+      <StyledCard>
+        <p>Safe address: {account}</p>
+        <p>Staking screen</p>
+      </StyledCard>
     </Section>
   );
 };
