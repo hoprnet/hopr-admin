c814c7bcd643ba0c524ec89ed37c9bccec2b3ad2
camilovegag <camilovegag@unisabana.edu.co>
2023-07-24 14:44:26 -0500

    Create Staking hub landing page

--- src/router.tsx
+++ src/router.tsx
@@ -64,6 +64,7 @@ import ConnectSafe from './components/ConnectSafe';
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import SafeOnboarding from './steps/safeOnboarding';
 import NoNodeAdded from './sections/noNodeAdded';
+import StakingLandingPage from './sections/stakingLandingPage';
 
 export type ApplicationMapType = {
   groupName: string;
@@ -217,6 +218,13 @@ export const applicationMapWeb3: ApplicationMapType = [
         path: 'safe/staking',
         icon: <SavingsIcon />,
         element: <SafeStakingPage />,
+        loginNeeded: 'web3',
+      },
+      {
+        name: 'Staking Hub',
+        path: 'staking-hub-landing',
+        icon: <SavingsIcon />,
+        element: <StakingLandingPage />,
         loginNeeded: 'web3',
       },
       {
