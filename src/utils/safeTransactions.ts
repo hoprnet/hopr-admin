6778fb537088acb63ca64bf54d2f2e1905cdf9ec
Diego Alzate <diego35.da@gmail.com>
2023-08-07 16:26:48 -0500

    add execute with 1 threshold

--- src/utils/safeTransactions.ts
+++ src/utils/safeTransactions.ts
@@ -1,10 +1,15 @@
-import { AllTransactionsListResponse, EthereumTxWithTransfersResponse, SafeModuleTransactionWithTransfersResponse, SafeMultisigTransactionWithTransfersResponse } from '@safe-global/api-kit'
+import { AllTransactionsListResponse, EthereumTxWithTransfersResponse, SafeModuleTransactionWithTransfersResponse, SafeMultisigTransactionWithTransfersResponse } from '@safe-global/api-kit';
 import { SafeMultisigTransactionResponse } from '@safe-global/safe-core-sdk-types';
 import { Address, decodeFunctionData, formatEther, formatUnits } from 'viem';
 import { erc20ABI, erc4626ABI, erc721ABI } from 'wagmi';
 import safeABI from '../abi/safeAbi.json';
 import { truncateEthereumAddress } from './blockchain';
 
+/**
+ * Pending transactions
+ */
+
+/** Human readable explanation of what the transaction is going to do */
 export const getRequestOfPendingTransaction = (transaction: SafeMultisigTransactionResponse) => {
   if (transaction.data) {
     try {
@@ -41,6 +46,38 @@ export const getSourceOfPendingTransaction = (transaction: SafeMultisigTransacti
   return truncateEthereumAddress(transaction.confirmations.at(0)?.owner ?? '');
 };
 
+export const getUserActionForPendingTransaction = (transaction: SafeMultisigTransactionResponse, ownerAddress: string): "EXECUTE" | "SIGN" | null => {
+  if (!ownerAddress) return null
+
+  const transactionHasEnoughSignatures = (transaction.confirmations?.length ?? 0) >= transaction.confirmationsRequired;
+
+  if (transactionHasEnoughSignatures) {
+    return 'EXECUTE'
+  }
+
+  const ownerHasSignedTransaction = transaction?.confirmations?.find((confirmation) => confirmation.owner === ownerAddress);
+
+  if (ownerHasSignedTransaction) {
+    // transaction does not have enough signatures and owner has already signed
+    // can only wait for more signatures
+    return null
+  }
+
+  const oneSignaturePending = transaction.confirmationsRequired - (transaction.confirmations?.length ?? 0)
+
+  if (oneSignaturePending) {
+    return 'EXECUTE'
+  }
+
+  // more than 1 signature is pending and owner has not signed
+  return 'SIGN'
+}
+
+
+/**
+ * Ethereum transactions
+ */
+
 const getValueFromHistoryEthereumTransaction = (transaction: EthereumTxWithTransfersResponse) => {
   if (!transaction.transfers.at(0)?.tokenAddress) {
     // native transfer
@@ -66,6 +103,10 @@ const getSourceFromHistoryEthereumTransaction = (transaction: EthereumTxWithTran
   return source;
 };
 
+/**
+ * Module transactions
+ */
+
 const getValueFromHistoryModuleTransaction = (transaction: SafeModuleTransactionWithTransfersResponse) => {
   const units = transaction.transfers.at(0)?.tokenInfo.decimals ?? 18;
   const value = formatUnits(BigInt(transaction.transfers.at(0)?.value ?? 0), units);
@@ -76,6 +117,10 @@ const getCurrencyFromHistoryModuleTransaction = (transaction: SafeModuleTransact
   const currency = transaction.transfers.at(0)?.tokenInfo.symbol;
   return currency;
 };
+
+/**
+ * Multisig transactions
+ */
 
 const getValueFromHistoryMultisigTransaction = (transaction: SafeMultisigTransactionWithTransfersResponse) => {
   const value = formatEther(BigInt(transaction.transfers.at(0)?.value ?? 0));
@@ -117,6 +162,10 @@ const getRequestFromHistoryMultisigTransaction = (transaction: SafeMultisigTrans
     return 'Rejection';
   }
 };
+
+/**
+ * History transactions (MULTISIG/MODULE/ETHEREUM)
+ */
 
 export const getSourceFromHistoryTransaction = (transaction: AllTransactionsListResponse['results']['0']) => {
   if (transaction.txType === 'ETHEREUM_TRANSACTION') {
