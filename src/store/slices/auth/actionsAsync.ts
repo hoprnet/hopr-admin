7d52a5c18997d697ebc9fb4634482dc61e8d5bac
Diego Alzate <diego35.da@gmail.com>
2023-08-08 09:58:57 -0500

    fix auth async actions

--- src/store/slices/auth/actionsAsync.ts
+++ src/store/slices/auth/actionsAsync.ts
@@ -1,57 +1,59 @@
 import { ActionReducerMapBuilder, createAsyncThunk } from '@reduxjs/toolkit';
 import { initialState } from './initialState';
-import { api } from '@hoprnet/hopr-sdk';
+import { GetInfoResponseType, api } from '@hoprnet/hopr-sdk';
 import { nodeActionsAsync } from '../node';
 import { parseEther } from 'viem';
+import { RootState } from '../..';
 const { getInfo } = api;
 
-export const loginThunk = createAsyncThunk(
-  'auth/login',
-  async (loginData: { apiToken: string; apiEndpoint: string }, {
-    rejectWithValue,
-    dispatch,
-  }) => {
-    const {
-      apiEndpoint,
-      apiToken,
-    } = loginData;
-    try {
-      const info = await getInfo({
-        apiEndpoint: apiEndpoint,
-        apiToken: apiToken,
-      });
-      return info;
-    } catch (e) {
-      // see if connecting error is due to low balance
-      const nodeBalances = await dispatch(
-        nodeActionsAsync.getBalancesThunk({
-          apiEndpoint,
-          apiToken,
-        }),
-      ).unwrap();
+export const loginThunk = createAsyncThunk<
+  GetInfoResponseType | undefined,
+  { apiToken: string; apiEndpoint: string },
+  { state: RootState }
+>('auth/login', async (loginData: { apiToken: string; apiEndpoint: string }, {
+  rejectWithValue,
+  dispatch,
+}) => {
+  const {
+    apiEndpoint,
+    apiToken,
+  } = loginData;
+  try {
+    const info = await getInfo({
+      apiEndpoint: apiEndpoint,
+      apiToken: apiToken,
+    });
+    return info;
+  } catch (e) {
+    // see if connecting error is due to low balance
+    const nodeBalances = await dispatch(
+      nodeActionsAsync.getBalancesThunk({
+        apiEndpoint,
+        apiToken,
+      }),
+    ).unwrap();
 
-      const addresses = await dispatch(
-        nodeActionsAsync.getAddressesThunk({
-          apiToken,
-          apiEndpoint,
-        }),
-      ).unwrap();
+    const addresses = await dispatch(
+      nodeActionsAsync.getAddressesThunk({
+        apiToken,
+        apiEndpoint,
+      }),
+    ).unwrap();
 
-      const minimumNodeBalance = parseEther('0.001');
+    const minimumNodeBalance = parseEther('0.001');
 
-      if (nodeBalances?.native && BigInt(nodeBalances.native) < minimumNodeBalance) {
-        return rejectWithValue(`Your xDai balance seems to low to operate the node. 
+    if (nodeBalances?.native && BigInt(nodeBalances.native) < minimumNodeBalance) {
+      return rejectWithValue(`Your xDai balance seems to low to operate the node. 
         \n Please top up your node.
         \n Address: ${addresses?.native}`);
-      }
-
-      return rejectWithValue(e);
     }
-  },
-);
+
+    return rejectWithValue(e);
+  }
+});
 
 export const createExtraReducers = (builder: ActionReducerMapBuilder<typeof initialState>) => {
-  builder.addCase(loginThunk.pending, (state, meta) => {
+  builder.addCase(loginThunk.pending, (state) => {
     state.status.connecting = true;
     state.status.connected = false;
     state.status.error = null;
