be13445840b91ba4baf15b21d3b28ea273535c4f
Michał Jadach <sarah.smith@email.com>
2023-08-14 13:18:46 +0200

    build process made better

--- scripts/prepare-build-hub.js
+++ scripts/prepare-build-hub.js
@@ -1,11 +1,22 @@
 var fs = require('fs');
 configPath = `${__dirname.replace('scripts', 'config.ts')}`;
 
-fs.readFile(configPath, function(err, data) {
-    if(err) throw err;
-    data = data.toString();
-    data = data.replace(` = 'dev';`, ` = 'web3';`);
-    fs.writeFile(configPath, data, function(err) {
-        err || console.log('Data replaced');
-    });
+fs.copyFile(configPath, `${configPath}_`, (err) => {
+  if (err) {
+      console.log("Error Found:", err);
+  } else {
+      console.log("Config backed up.");
+
+      fs.readFile(configPath, function(err, data) {
+          if(err) throw err;
+          data = data.toString();
+          data = data.replace(`export const environment:('dev' | 'node' | 'web3') = `, `export const environment:('dev' | 'node' | 'web3') = 'web3'; //`);
+          fs.writeFile(configPath, data, function(err) {
+              err || console.log('Data replaced to build only Staking Hub.');
+          });
+      });
+
+  }
 });
+  
+
