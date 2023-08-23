d21300838d32ddbbfaef20159949c594f100d60d
Michał Jadach <sarah.smith@email.com>
2023-08-23 15:56:04 +0200

    onboarding page

---
+++ src/pages/staking-hub/onboarding.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+// HOPR Components
+import Section from '../../future-hopr-lib-components/Section';
+
+
+function Onboarding() {
+  return (
+    <Section
+        lightBlue
+        center
+        fullHeightMin
+    >
+
+            sa
+
+    </Section>
+  );
+}
+
+export default Onboarding;
