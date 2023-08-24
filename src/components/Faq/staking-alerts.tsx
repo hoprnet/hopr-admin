8172c331286069888e4a4ce569968118a22449ab
Luis Martínez <luis.martinez@hoprnet.org>
2023-08-24 01:58:38 -0500

    Staking FAQ (#231)
    * Add Staking FAQ
    
    * Update FAQ links to be clickable and look different than other text

---
+++ src/components/Faq/staking-alerts.tsx
@@ -0,0 +1,18 @@
+type FaqElement = {
+  id: number;
+  title: string;
+  content: string | JSX.Element;
+};
+
+type FaqData = Record<string, FaqElement[]>;
+
+const stakingAlerts: FaqData = { '/steps/set-allowance': [
+  {
+    id: 1,
+    title: 'Funds at risk',
+    content:
+        'If your node is ever compromised it may lead to your entire allowance of wxHOPR being drained. Please set a reasonable allowance, and use our default value as a guideline.',
+  },
+] };
+
+export default stakingAlerts;
