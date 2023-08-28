df77ea7cfd64118de818d19c1aae38f7fba26bb2
Diego Alzate <diego35.da@gmail.com>
2023-08-28 09:21:20 -0500

    add copy to set allowance

--- src/pages/staking-hub/onboarding/step8/0setAllowance.tsx
+++ src/pages/staking-hub/onboarding/step8/0setAllowance.tsx
@@ -6,14 +6,26 @@ import { useEthersSigner } from '../../../../hooks';
 import { StepContainer } from '../components';
 
 // Blockchain
-import { MAX_UINT256, createApproveTransactionData } from '../../../../utils/blockchain';
+import { Address, formatEther, parseEther } from 'viem';
 import { HOPR_CHANNELS_SMART_CONTRACT_ADDRESS, HOPR_TOKEN_USED_CONTRACT_ADDRESS } from '../../../../../config';
-import { Address } from 'viem';
+import { MAX_UINT256, createApproveTransactionData } from '../../../../utils/blockchain';
 
 // Store
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../store';
 import { safeActionsAsync } from '../../../../store/slices/safe';
 import { stakingHubActions } from '../../../../store/slices/stakingHub';
+import { Lowercase, StyledCoinLabel, StyledInputGroup, StyledTextField } from '../safeOnboarding/styled'
+
+
+const StyledText = styled.h3`
+  color: var(--414141, #414141);
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  letter-spacing: 0.35px;
+  text-align: end;
+`;
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -30,25 +42,46 @@ export default function SetAllowance() {
   const selectedSafeAddress = useAppSelector((store) => store.safe.selectedSafeAddress.data) as Address;
   const isLoading = useAppSelector((store) => store.safe.createTransaction.isFetching);
   const signer = useEthersSigner();
+  const [wxhoprValue, set_wxhoprValue] = useState('');
 
   const setAllowance = async () => {
     if (signer && selectedSafeAddress) {
-      dispatch(
+      await dispatch(
         safeActionsAsync.createSafeContractTransaction({
           data: createApproveTransactionData(HOPR_CHANNELS_SMART_CONTRACT_ADDRESS, MAX_UINT256),
           signer,
           safeAddress: selectedSafeAddress,
           smartContractAddress: HOPR_TOKEN_USED_CONTRACT_ADDRESS,
         }),
-      );
+      ).unwrap();
     }
   };
 
   return (
     <StepContainer
-      title="Set Allowance"
-      description={''}
+      title="SET wxHOPR ALLOWANCE"
+      description={`Your node will need to access wxHOPR from your safe to fund payment channels on the HOPR network. You can set a maximum allowance to reduce your funds at risk in case your node is ever compromised.`}
     >
+      <StyledInputGroup>
+        <StyledText>NODE ALLOWANCE</StyledText>
+        <StyledTextField
+          type="number"
+          variant="outlined"
+          placeholder="-"
+          size="small"
+          value={formatEther(BigInt(wxhoprValue))}
+          onChange={(e) => set_wxhoprValue(parseEther(e.target.value).toString())}
+          InputProps={{ inputProps: {
+            style: { textAlign: 'right' },
+            min: 0,
+            pattern: '[0-9]*',
+          } }}
+        />
+        <StyledCoinLabel>
+          <Lowercase>wx</Lowercase>hopr
+        </StyledCoinLabel>
+        <Button onClick={() => set_wxhoprValue(MAX_UINT256.toString())}>DEFAULT</Button>
+      </StyledInputGroup>
       <ButtonContainer>
         <StyledGrayButton
           onClick={() => {
