2b25b8155568ff4079a43f003cbf4d50e0e9647c
Luis Martínez <luis.martinez@hoprnet.org>
2023-08-29 12:24:46 -0500

    Remove old code

--- src/pages/staking-hub/onboarding/step3/0selectNodeType.tsx
+++ src/pages/staking-hub/onboarding/step3/0selectNodeType.tsx
@@ -11,20 +11,6 @@ import MuiButton from '@mui/material/Button';
 import { Radio } from '@mui/material';
 import { useState } from 'react';
 
-const NodeContainer = styled.div`
-  display: flex;
-  gap: 2.5rem;
-  justify-content: center;
-`;
-
-const Node = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  gap: 1rem;
-  max-width: 320px;
-`;
-
 const ImageContainer = styled.div`
   display: flex;
   align-items: center;
@@ -53,21 +39,6 @@ const Price = styled(Title)`
   &:empty {
     height: 42px;
   }
-`;
-
-const TextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  height: 160px;
-  gap: 0.5rem;
-`;
-
-const Stack = styled.div`
-  margin-top: auto;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
 `;
 
 const StyledButton = styled(Button)`
@@ -139,52 +110,6 @@ export default function SelectNodeType() {
 
   return (
     <StepContainer title="SELECT NODE TYPE">
-      {/* <NodeContainer>
-        <Node>
-          <ImageContainer>
-            <Image src="/assets/docker.svg" />
-          </ImageContainer>
-          <Title>Docker Version</Title>
-          <Price>Free</Price>
-          <TextContainer>
-            If you want to run a node on your own hardware without the HOPR Node PC, that’s perfectly acceptable and
-            possible as well!
-          </TextContainer>
-        </Node>
-        <Node>
-          <ImageContainer>
-            <Image
-              src="/images/dappnode.png"
-              width={310}
-            />
-          </ImageContainer>
-          <Title>Dappnode</Title>
-          <Price>$ 1,630.70</Price>
-          <TextContainer>
-            <span>
-              <b>CPU:</b> Intel NUC i7 Series 10
-            </span>
-            <span>
-              <b>Memory:</b> 32gb
-            </span>
-            <span>
-              <b>Storage:</b> 2TB NVMe
-            </span>
-            <br />
-            <span>
-              <b>Bonus tokens:</b> 1000 HOPR tokens
-            </span>
-          </TextContainer>
-          <Stack>
-            <StyledButton
-              href="https://dappnode.com/collections/frontpage/products/hopr-special-edition"
-              target="_blank"
-            >
-              Order
-            </StyledButton>
-          </Stack>
-        </Node>
-      </NodeContainer> */}
       <OptionContainer>
         <Option
           className={`${option === 0 ? 'chosen' : ''}`}
