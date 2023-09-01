867f5323acc9dc7170b9b1cf22c66666c44893b5
mjadach-iv <104129084+mjadach-iv@users.noreply.github.com>
2023-09-01 09:16:16 +0200

    Dashboard in place (#307)
    * Dashboard
    
    * added docs to drawer
    
    * version bump
    
    * remove comment
    
    ---------
    
    Co-authored-by: Michał Jadach <sarah.smith@email.com>

--- src/router.tsx
+++ src/router.tsx
@@ -24,11 +24,11 @@ import MetricsPage from './pages/node/metrics';
 import SafeStakingPage from './pages/staking-hub/safeStaking';
 import ConfigurationPage from './pages/node/configuration';
 import WrapperPage from './pages/staking-hub/wrapper';
-import StakingScreen from './pages/staking-hub/staking-screen';
+import StakingScreen from './pages/staking-hub/dashboard/staking';
 import SafeWithdraw from './pages/staking-hub/safeWithdraw';
-import NodeAdded from './pages/staking-hub/nodeAdded';
-import SafeActions from './pages/staking-hub/actions';
-import NoNodeAdded from './pages/staking-hub/noNodeAdded';
+import NodeAdded from './pages/staking-hub/dashboard/node';
+import SafeActions from './pages/staking-hub/dashboard/transactions';
+import NoNodeAdded from './pages/staking-hub/dashboard/noNodeAdded';
 import Onboarding from './pages/staking-hub/onboarding';
 import Dashboard from './pages/staking-hub/dashboard'
 
@@ -47,7 +47,6 @@ import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import SettingsIcon from '@mui/icons-material/Settings';
 import TerminalIcon from '@mui/icons-material/Terminal';
 import MailIcon from '@mui/icons-material/Mail';
-import HubIcon from '@mui/icons-material/Hub';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import LockIcon from '@mui/icons-material/Lock';
 import ContactPhone from '@mui/icons-material/ContactPhone';
@@ -56,13 +55,11 @@ import NodeIcon from '@mui/icons-material/Router';
 import NetworkingIcon from '@mui/icons-material/Diversity3';
 import DevelopIcon from '@mui/icons-material/Code';
 import BarChartIcon from '@mui/icons-material/BarChart';
-import SwapVertIcon from '@mui/icons-material/SwapVert';
-import AddBoxIcon from '@mui/icons-material/AddBox';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
 import PaidIcon from '@mui/icons-material/Paid';
 import WalletIcon from '@mui/icons-material/Wallet';
 import IncomingChannelsIcon from './future-hopr-lib-components/Icons/IncomingChannels';
 import OutgoingChannelsIcon from './future-hopr-lib-components/Icons/OutgoingChannels';
-import WavingHandIcon from '@mui/icons-material/WavingHand';
 import TrainIcon from './future-hopr-lib-components/Icons/TrainIcon';
 import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
 
@@ -209,7 +206,20 @@ export const applicationMapStakingHub: ApplicationMapType = [
       },
     ],
   },
+  {
+    groupName: 'RESOURCES',
+    path: 'networking',
+    icon: <NetworkingIcon />,
+    items: [
+      {
+        name: 'Docs',
+        path: 'https://docs.hoprnet.org/',
+        icon: <DescriptionOutlinedIcon />,
+      },
+    ]
+  }
 ];
+
 
 export const applicationMapDevWeb3: ApplicationMapType = [
   {
@@ -231,41 +241,6 @@ export const applicationMapDevWeb3: ApplicationMapType = [
         element: <SectionSafe />,
         loginNeeded: 'web3',
       },
-      {
-        name: 'Staking screen',
-        path: 'staking-screen',
-        icon: <SavingsIcon />,
-        element: <StakingScreen />,
-        loginNeeded: 'web3',
-      },
-      {
-        name: 'No node added',
-        path: 'no-node',
-        icon: <SavingsIcon />,
-        element: <NoNodeAdded />,
-        loginNeeded: 'web3',
-      },
-      {
-        name: 'Node added',
-        path: 'node-added',
-        icon: <SavingsIcon />,
-        element: <NodeAdded />,
-        loginNeeded: 'web3',
-      },
-      {
-        name: 'Actions',
-        path: 'safe/actions',
-        icon: <SwapVertIcon />,
-        element: <SafeActions />,
-        loginNeeded: 'web3',
-      },
-      {
-        name: 'Safe/Staking',
-        path: 'safe/staking',
-        icon: <SavingsIcon />,
-        element: <SafeStakingPage />,
-        loginNeeded: 'web3',
-      },
     ],
   },
 ];
@@ -284,7 +259,7 @@ function createApplicationMap() {
   if (environment === 'dev' || environment === 'node') applicationMapNode.map((elem) => temp.push(elem));
   if (environment === 'dev' || environment === 'web3') applicationMapStakingHub.map((elem) => temp.push(elem));
   if (environment === 'dev' || environment === 'web3') applicationMapDevWeb3.map((elem) => temp.push(elem));
-  if (environment === 'dev') applicationMapDev.map((elem) => temp.push(elem));
+  //if (environment === 'dev') applicationMapDev.map((elem) => temp.push(elem));
   return temp;
 }
 
