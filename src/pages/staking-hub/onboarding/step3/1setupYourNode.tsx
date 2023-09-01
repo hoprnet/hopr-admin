e998de6b7d0cfda0fcb44802c2310cad637b6001
Michał Jadach <sarah.smith@email.com>
2023-09-01 12:25:52 +0200

    fix typo

--- src/pages/staking-hub/onboarding/step3/1setupYourNode.tsx
+++ src/pages/staking-hub/onboarding/step3/1setupYourNode.tsx
@@ -125,7 +125,7 @@ export default function SetupNodeStep() {
               >here</StyledLink>.</p>
               <Content>
                 <CodeContainer>
-                  <span>INSTALL HOPRd</span>
+                  <span>INSTALL AND RUN HOPRd</span>
                   <CodeCopyBox
                     code={<>{`docker run --pull always --restart on-failure -m 2g --log-driver json-file --log-opt max-size=100M --log-opt max-file=5 -ti -v $HOME/.hoprd-db-dufour:/app/hoprd-db -p 9091:9091/tcp -p 9091:9091/udp -p 8080:8080 -p 3001:3001 -e DEBUG="hopr*" europe-west3-docker.pkg.dev/hoprassociation/docker-images/hoprd:latest --network dufour --init --api --identity /app/hoprd-db/.hopr-id-dufour --data /app/hoprd-db --password 'open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0' --apiHost "0.0.0.0" --apiToken '`}
                     <span style={{color: '#00fc00'}}>YOUR_SECURITY_TOKEN</span>{`' --healthCheck --healthCheckHost "0.0.0.0" --announce --safeAddress ${safeAddress} --moduleAddress ${moduleAddress} --host `}<span style={{color: '#00fc00'}}>{`<YOUR_PUBLIC_IP>`}</span>{`:9091`}</>}
