679ce5b498ab9a147f07109e711087c596d5ecf3
Andrius <73285987+Jaguaras@users.noreply.github.com>
2023-09-03 15:10:42 +0300

    Update 1safeIsFunded.tsx

--- src/pages/staking-hub/onboarding/step2/1safeIsFunded.tsx
+++ src/pages/staking-hub/onboarding/step2/1safeIsFunded.tsx
@@ -9,7 +9,7 @@ export default function safeIsReady() {
   const dispatch = useAppDispatch();
   return (
     <StepContainer
-      title="Funds successfully transferred to Safe. Now let's set up your HOPR node!"
+      description={'Funds successfully transferred to Safe. Now let's set up your HOPR node!'}
       image={{
         src: '/assets/safe-success-2.svg',
         alt: 'Safe deployed successfully',
