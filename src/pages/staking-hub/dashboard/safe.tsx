d660ba0f43ee83d4d3ab85dab774feed401e2009
Diego Alzate <diego35.da@gmail.com>
2023-09-11 10:04:58 -0500

    WIP: multisend

--- src/pages/staking-hub/dashboard/safe.tsx
+++ src/pages/staking-hub/dashboard/safe.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
+import { useState } from 'react';
 import { useEthersSigner } from '../../../hooks';
 import { useAppDispatch, useAppSelector } from '../../../store';
 import { safeActionsAsync } from '../../../store/slices/safe';
 
 // HOPR Components
+import { OperationType } from '@safe-global/safe-core-sdk-types';
 import Button from '../../../future-hopr-lib-components/Button';
 import { GrayCard } from '../../../future-hopr-lib-components/Cards/GrayCard';
 
@@ -52,34 +53,36 @@ const Container = styled.div`
 function SafeDashboard() {
   const dispatch = useAppDispatch();
   const signer = useEthersSigner();
-
   const selectedSafeAddress = useAppSelector((store) => store.safe.selectedSafeAddress.data) as `0x${string}`;
+  const moduleAddress = useAppSelector((store) => store.stakingHub.onboarding.moduleAddress)
   const needsUpdate = useAppSelector((store) => store.stakingHub.config.needsUpdate.data);
   const [updating, set_updating] = useState(false)
 
 
   const updateConfig = async () => {
-    
 
-    if (signer) {
-        set_updating(true);
+    if (signer && moduleAddress) {
+      set_updating(true);
+      const moduleAddressWithout0x = moduleAddress.slice(2).toLocaleLowerCase()
+      console.log({ moduleAddress, moduleAddressWithout0x })
+      const newConfig = `0x8d80ff0a0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000016b00${moduleAddressWithout0x}000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000243401cde8000000000000000000000000693bac5ce61c720ddc68533991ceb41199d8f8ae00${moduleAddressWithout0x}000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000243401cde8000000000000000000000000d4fdec44db9d44b8f2b6d529620f9c0c7066a2c100${moduleAddressWithout0x}00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000024a2450f89693bac5ce61c720ddc68533991ceb41199d8f8ae010103030303030303030303000000000000000000000000000000000000000000`;
 
-        const newConfig = `0x8d80ff0a0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000016b00`;
-
-        dispatch(
-            safeActionsAsync.createAndExecuteContractTransactionThunk({
-                data: newConfig,
-                signer,
-                safeAddress: selectedSafeAddress,
-                smartContractAddress: '0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526', //multisend contract
-            }),
-        )
+      dispatch(
+        safeActionsAsync.createAndExecuteContractTransactionThunk({
+          data: newConfig,
+          signer,
+          safeAddress: selectedSafeAddress,
+          operation: OperationType.DelegateCall,
+          smartContractAddress: '0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526', //multisend contract
+        }),
+      )
         .unwrap()
         .then((transactionResponse) => {
-       //     set_proposedTxHash(transactionResponse);
+          console.log(`transaction went through`)
+          //     set_proposedTxHash(transactionResponse);
         })
         .finally(() => {
-            set_updating(false);
+          set_updating(false);
         });
     }
 
@@ -89,39 +92,39 @@ function SafeDashboard() {
   
   return (
     <Container
-        className='SafeDashboard'
+      className='SafeDashboard'
     >
-        <div className='half-line'>
-          <GrayCard
-            id="Update-Node-Configuration"
-            title="Safe Configuration"
-            currency={needsUpdate ? <span style={{color: 'red'}}>Update needed</span> : <span>Current version</span>}
-            buttons={[
-              {
-                text: 'UPDATE',
-                onClick: updateConfig,
-                pending: updating,
-                disabled: !needsUpdate,
-              },
-            ]}
-          />
-        </div>
+      <div className='half-line'>
+        <GrayCard
+          id="Update-Node-Configuration"
+          title="Safe Configuration"
+          currency={needsUpdate ? <span style={{ color: 'red' }}>Update needed</span> : <span>Current version</span>}
+          buttons={[
+            {
+              text: 'UPDATE',
+              onClick: updateConfig,
+              pending: updating,
+              disabled: !needsUpdate,
+            },
+          ]}
+        />
+      </div>
 
-        <p className='center'>In order to adjust the settings of your safe or transfer assets that are not supported by the HOPR Staking Hub use any of the below mentioned third party general purpose Safe user interfaces:</p>
-        <div className='center'>
-            <Button
+      <p className='center'>In order to adjust the settings of your safe or transfer assets that are not supported by the HOPR Staking Hub use any of the below mentioned third party general purpose Safe user interfaces:</p>
+      <div className='center'>
+        <Button
 
-                href={`https://app.safe.global/home?safe=gno:${selectedSafeAddress}`}
-            >
+          href={`https://app.safe.global/home?safe=gno:${selectedSafeAddress}`}
+        >
                 safe.global
-            </Button>
-            <Button
+        </Button>
+        <Button
 
-                href={`https://app.onchainden.com/safes/gnosis:${selectedSafeAddress}`}
-            >
+          href={`https://app.onchainden.com/safes/gnosis:${selectedSafeAddress}`}
+        >
                 OnChainDen.com
-            </Button>
-        </div>
+        </Button>
+      </div>
     
     </Container>
   );
