c396cec8f634521539420b87a83c69cbe3d6adf3
Luis Martínez <luis.martinez@hoprnet.org>
2023-09-15 11:50:52 -0500

    Change loadStateFromLocalStorage return type from string to unkown

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -2,7 +2,7 @@ export function saveStateToLocalStorage(key: string, state: unknown) {
     localStorage.setItem(key, JSON.stringify(state));
 }
 
-export function loadStateFromLocalStorage(key: string): string | null {
+export function loadStateFromLocalStorage(key: string): unknown | null {
     const storedState = localStorage.getItem(key);
     return storedState ? JSON.parse(storedState) : null;
 }
