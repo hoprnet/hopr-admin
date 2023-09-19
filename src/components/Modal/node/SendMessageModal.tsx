1a7e05a494649672aca95453add212d483b46397
Luis Martínez <luis.martinez@hoprnet.org>
2023-09-18 21:01:59 -0500

    WIP: Add style for directMessage and Path

--- src/components/Modal/node/SendMessageModal.tsx
+++ src/components/Modal/node/SendMessageModal.tsx
@@ -6,6 +6,7 @@ import { SDialog, SDialogContent, SIconButton, TopBar } from '../../../future-ho
 import Checkbox from '../../../future-hopr-lib-components/Toggles/Checkbox';
 import IconButton from '../../../future-hopr-lib-components/Button/IconButton';
 import Button from '../../../future-hopr-lib-components/Button';
+// import Select from '../../../future-hopr-lib-components/Select';
 
 // Mui
 import {
@@ -13,7 +14,10 @@ import {
   DialogActions,
   CircularProgress,
   TextField,
-  Tooltip
+  Tooltip,
+  SelectChangeEvent,
+  Select,
+  MenuItem
 } from '@mui/material'
 
 import { SendMessagePayloadType } from '@hoprnet/hopr-sdk';
@@ -29,7 +33,25 @@ const PathOrHops = styled.div`
   flex-direction: row;
   gap: 8px;
   align-items: center;
+  width: 100%;
+
+  .directMessageSelected{
+    justify-content: center;
+  }
+
+  .pathSelected {
+    justify-content: flex-start;
+  }
+
+  .directMessage {
+    flex: 1;
+  }
+
+  .path {
+    flex: 0.5;
+  }
 `;
+
 
 const StatusContainer = styled.div`
   height: 32px;
@@ -46,7 +68,8 @@ export const SendMessageModal = (props: SendMessageModalProps) => {
   const [loader, set_loader] = useState<boolean>(false);
   const [status, set_status] = useState<string>('');
   const [numberOfHops, set_numberOfHops] = useState<number | ''>('');
-  const [sendMode, set_sendMode] = useState<'path' | 'automaticPath' | 'numberOfHops' | 'directMessage' | 'none'>('directMessage');
+  // const [sendMode, set_sendMode] = useState<'path' | 'automaticPath' | 'numberOfHops' | 'directMessage' | 'none'>('directMessage');
+  const [sendMode, set_sendMode] = useState('directMessage');
   const [message, set_message] = useState<string>('');
   const [receiver, set_receiver] = useState<string>(props.peerId ? props.peerId : '');
   const [openModal, set_openModal] = useState<boolean>(false);
@@ -113,6 +136,10 @@ export const SendMessageModal = (props: SendMessageModalProps) => {
       });
   };
 
+  const handleSendModeChange = (event: SelectChangeEvent) => {
+    set_sendMode(event.target.value as string);
+  }
+
   const handleDirectMessage = () => {
     if (sendMode === 'directMessage') {
       return set_sendMode('none');
@@ -135,7 +162,6 @@ export const SendMessageModal = (props: SendMessageModalProps) => {
   };
 
   const handleNumberOfHops = (event: React.ChangeEvent<HTMLInputElement>) => {
-    set_sendMode('numberOfHops');
     set_numberOfHops(
       parseInt(event.target.value) || parseInt(event.target.value) === 0 ? parseInt(event.target.value) : '',
     );
@@ -153,6 +179,13 @@ export const SendMessageModal = (props: SendMessageModalProps) => {
     set_path('');
     set_openModal(false);
     set_status('');
+  };
+
+  const handlePathKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === ' ' || e.key === 'Spacebar') {
+      e.preventDefault(); // Prevent space key from inserting a space
+      set_path((prevPath) => prevPath + '\n'); // Insert a new line character
+    }
   };
 
   const isAlias = (alias: string) => {
@@ -222,7 +255,46 @@ export const SendMessageModal = (props: SendMessageModalProps) => {
             fullWidth
           />
           <span style={{ margin: '0px 0px -6px' }}>Send mode:</span>
-          <PathOrHops>
+          <PathOrHops className={sendMode === "path" ? "pathSelected" : "directMessageSelected"}>
+            <Select
+              value={sendMode}
+              label="Send Mode"
+              onChange={handleSendModeChange}
+              className={sendMode === "directMessage" ? "directMessage" : "path"}
+            >
+              <MenuItem value="directMessage">Direct Message</MenuItem>
+              <MenuItem value="path">Path</MenuItem>
+            </Select>
+            {sendMode === 'directMessage' && (
+              <TextField
+                type="number"
+                label="Number of Hops"
+                placeholder="1"
+                value={numberOfHops}
+                onChange={handleNumberOfHops}
+                inputProps={{
+                  min: 0,
+                  max: 10,
+                  step: 1,
+                }}
+                style={{ flex: 1 }}
+                fullWidth
+              />
+            )}
+          </PathOrHops>
+          {sendMode === 'path' && (
+            <TextField
+              label="Path"
+              placeholder="16Uiu2...9cTYntS3, 16Uiu2...9cDFSAa"
+              value={path}
+              onChange={handlePath}
+              onKeyDown={handlePathKeyDown}
+              multiline
+              rows={4}
+              fullWidth
+            />
+          )}
+          {/* <PathOrHops>
             <Checkbox
               label="Direct message"
               value={sendMode === 'directMessage'}
@@ -275,7 +347,8 @@ export const SendMessageModal = (props: SendMessageModalProps) => {
                 sx={{ maxWidth: '240px' }}
               />
             </Tooltip>
-          </PathOrHops>
+          </PathOrHops> */}
+
         </SDialogContent>
         <DialogActions>
           <Button
@@ -298,4 +371,135 @@ export const SendMessageModal = (props: SendMessageModalProps) => {
       </SDialog>
     </>
   );
+
+  // return (
+  //   <>
+  //     <IconButton
+  //       iconComponent={<ForwardToInboxIcon />}
+  //       tooltipText={
+  //         <span>
+  //           SEND
+  //           <br />
+  //           message
+  //         </span>
+  //       }
+  //       onClick={handleOpenModal}
+  //       disabled={props.disabled}
+  //     />
+
+  //     <SDialog
+  //       open={openModal}
+  //       onClose={handleCloseModal}
+  //       disableScrollLock={true}
+  //       maxWidth={'800px'}
+  //     >
+  //       <TopBar>
+  //         <DialogTitle>Send Message</DialogTitle>
+  //         <SIconButton
+  //           aria-label="close modal"
+  //           onClick={handleCloseModal}
+  //         >
+  //           <CloseIcon />
+  //         </SIconButton>
+  //       </TopBar>
+  //       <SDialogContent
+  //       >
+  //         <TextField
+  //           label="Receiver (Peer Id)"
+  //           placeholder="16Uiu2..."
+  //           value={receiver}
+  //           onChange={(e) => set_receiver(e.target.value)}
+  //           required
+  //           fullWidth
+  //         />
+  //         <TextField
+  //           label="Message"
+  //           placeholder="Hello Node..."
+  //           multiline
+  //           maxRows={4}
+  //           rows={4}
+  //           value={message}
+  //           onChange={(e) => set_message(e.target.value)}
+  //           inputProps={{ maxLength: maxLength }}
+  //           helperText={`${remainingChars} characters remaining`}
+  //           required
+  //           fullWidth
+  //         />
+  //         <span style={{ margin: '0px 0px -6px' }}>Send mode:</span>
+  //         <PathOrHops>
+  //           <Checkbox
+  //             label="Direct message"
+  //             value={sendMode === 'directMessage'}
+  //             onChange={handleDirectMessage}
+  //           />
+  //           or
+  //           <Checkbox
+  //             label="Automatic path"
+  //             value={sendMode === 'automaticPath'}
+  //             onChange={handleAutomaticPath}
+  //           />
+  //           or
+  //           <Tooltip
+  //             title={nonAutomaticPathTooltip}
+  //             disableHoverListener={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //             disableFocusListener={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //             disableTouchListener={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //             disableInteractive={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //           >
+  //             <TextField
+  //               style={{ width: '180px' }}
+  //               type="number"
+  //               label="Number of Hops"
+  //               placeholder="1"
+  //               value={numberOfHops}
+  //               onChange={handleNumberOfHops}
+  //               inputProps={{
+  //                 min: 0,
+  //                 max: 10,
+  //                 step: 1,
+  //               }}
+  //               disabled={sendMode === 'automaticPath' || sendMode === 'directMessage'}
+  //             />
+  //           </Tooltip>
+  //           or
+  //           <Tooltip
+  //             title={nonAutomaticPathTooltip}
+  //             disableHoverListener={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //             disableFocusListener={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //             disableTouchListener={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //             disableInteractive={!(sendMode === 'automaticPath' || sendMode === 'directMessage')}
+  //           >
+  //             <TextField
+  //               label="Path"
+  //               placeholder="16Uiu2...9cTYntS3, 16Uiu2...9cDFSAa"
+  //               value={path}
+  //               onChange={handlePath}
+  //               disabled={sendMode === 'automaticPath' || sendMode === 'directMessage'}
+  //               fullWidth
+  //               sx={{ maxWidth: '240px' }}
+  //             />
+  //           </Tooltip>
+  //         </PathOrHops>
+  //       </SDialogContent>
+  //       <DialogActions>
+  //         <Button
+  //           onClick={handleSendMessage}
+  //           disabled={
+  //             sendMode !== 'directMessage' && (sendMode !== 'automaticPath' && numberOfHops === '' && path === '') || message.length === 0 || receiver.length === 0
+  //           }
+  //           style={{
+  //             width: '100%',
+  //             marginTop: '8px',
+  //           }}
+  //         >
+  //           Send
+  //         </Button>
+  //       </DialogActions>
+  //       <StatusContainer>
+  //         {loader && <CircularProgress />}
+  //         {status}
+  //       </StatusContainer>
+  //     </SDialog>
+  //   </>
+  // );
 };
