1e8270868266eb0f638804366d113242ec3de5d9
Michał Jadach <michal.jadach@hoprnet.org>
2023-11-15 16:34:28 +0100

    Info better page

---
+++ src/future-hopr-lib-components/Button/SmallActionButton.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Tooltip from '../Tooltip/tooltip-fixed-width';
+
+
+const SIconButton = styled(IconButton)`
+    width: 24px;
+    height: 24px;
+    svg {
+        width: 18px;
+        height: 18px;
+    }
+`;
+
+interface SmallActionButtonProps extends IconButtonProps {
+    tooltip: string;
+}
+
+
+function SmallActionButton(props: SmallActionButtonProps) {
+    return (
+        <Tooltip
+            title={props.tooltip}
+        >
+            <SIconButton>
+                {props.children}
+            </SIconButton>
+        </Tooltip>
+    )
+}
+
+export default SmallActionButton;
