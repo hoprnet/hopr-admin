f21dcc941dcb6b5bb2b1833d62b5b9201ab9a78f
Michał Jadach <michal.jadach@hoprnet.org>
2024-01-11 14:42:34 +0100

    cleanup

--- src/components/Modal/staking-hub/DockerRunCommandModal.tsx
+++ src/components/Modal/staking-hub/DockerRunCommandModal.tsx
@@ -1,16 +1,12 @@
-import { useState, useEffect } from 'react';
-import { DialogTitle, TextField, DialogActions, Alert } from '@mui/material';
+import { useState } from 'react';
+import { DialogTitle } from '@mui/material';
 import { SDialog, SDialogContent, SIconButton, TopBar } from '../../../future-hopr-lib-components/Modal/styled';
-import { useAppDispatch, useAppSelector } from '../../../store';
-import { actionsAsync } from '../../../store/slices/node/actionsAsync';
-import { appActions } from '../../../store/slices/app';
+import { useAppSelector } from '../../../store';
 import CloseIcon from '@mui/icons-material/Close';
-import { sendNotification } from '../../../hooks/useWatcher/notifications';
 import CodeIcon from '@mui/icons-material/Code';
 
 // HOPR Components
 import IconButton from '../../../future-hopr-lib-components/Button/IconButton';
-import AddAliasIcon from '../../../future-hopr-lib-components/Icons/AddAlias';
 import Button from '../../../future-hopr-lib-components/Button';
 import { CodeContainer } from '../../../pages/staking-hub/onboarding/step3/1setupYourNode';
 
@@ -20,8 +16,6 @@ type DockerRunCommandModalProps = {
 };
 
 export const DockerRunCommandModal = (props: DockerRunCommandModalProps) => {
-  const dispatch = useAppDispatch();
-  const [duplicateAlias, set_duplicateAlias] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const safeAddress = useAppSelector((store) => store.stakingHub.onboarding.safeAddress);
   const moduleAddress = useAppSelector((store) => store.stakingHub.onboarding.moduleAddress);
